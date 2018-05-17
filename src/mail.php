f2ae3b70cebceaaf544c39a7343d7d6e2ea13bee
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2018-05-17 20:57:31 +0700

    fix

--- src/mail.php
+++ src/mail.php
@@ -1,11 +1,11 @@
 
 <?php
-$to      = 'tai.traffic@gmail.com';
-$subject = 'Обратный звонок с heavensmoke';
-$message = 'Заявка.  Имя: ' . $_POST["name"] . ' Телефон: ' . $_POST["phone"] ;
-$headers = 'From: hello@heavensmoke.ru' . "\r\n" .
+$to      = 'spbgazon@gmail.com';
+$subject = 'Письмо с сайта Петербургские газоны';
+$message = 'Имя: ' . $_POST["name"] . ' Телефон: ' . $_POST["phone"] . ' Email: ' . $_POST["email"] . ' Размер участка: ' . $_POST["size"] ;
+$headers = 'From: hello@spbgazon.ru' . "\r\n" .
     'Content-Type: text/plain; charset=utf8;' . "\r\n" .
-    'Reply-To: hello@heavensmoke.ru' . "\r\n" .
+    'Reply-To: hello@spbgazon.ru' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 mail($to, $subject, $message, $headers);
 ?>
